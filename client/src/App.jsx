572163b02a9efc2038aaa036e567abc1ddf31d76
Jeeraphat <jeeraphat1742@gmail.com>
2024-03-19 00:22:20 +0700

    complete sign in page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import SignIn from './pages/Signin'
 
 export default function App() {
 	return (
@@ -13,7 +13,7 @@ export default function App() {
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/about" element={<About />}></Route>
-				<Route path="/sign-in" element={<Signin />}></Route>
+				<Route path="/sign-in" element={<SignIn />}></Route>
 				<Route path="/sign-up" element={<SignUp />}></Route>
 				<Route path="/profile" element={<Profile />}></Route>
 			</Routes>
