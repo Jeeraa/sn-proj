239220a14ad8cbe92ee8c19408878db1fe028296
Jeeraphat <jeeraphat1742@gmail.com>
2024-03-19 18:11:53 +0700

    create update user api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import { error } from 'console'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -19,6 +20,8 @@ mongoose
 const app = express()
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.listen(3000, () => {
 	console.log('Server listening on port 3000')
