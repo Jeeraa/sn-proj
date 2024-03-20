9a1fada913d2e3e2c0c6ae75779d7153c86ac00e
Jeeraphat <jeeraphat1742@gmail.com>
2024-03-20 13:14:33 +0700

    edit modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,3 +1,9 @@
+import {
+	ArrowPathIcon,
+	CheckCircleIcon,
+	ClockIcon,
+	CurrencyDollarIcon,
+} from '@heroicons/react/24/outline'
 import { useState } from 'react'
 
 export default function Modal({ isOpen, onClose, onUpdateStatus }) {
@@ -17,45 +23,54 @@ export default function Modal({ isOpen, onClose, onUpdateStatus }) {
 		>
 			<div className="fixed inset-0 bg-gray-900 opacity-50"></div>
 			<div className="bg-white p-8 rounded-lg shadow-lg z-50">
-				<h2 className="text-lg font-semibold mb-4">Update Status</h2>
-				<div className="flex flex-col space-y-4">
+				<h2 className="text-lg font-semibold mb-4 text-center">อัปเดตสถานะ</h2>
+				<div className="flex justify-between items-center space-x-5">
 					<button
-						className={`status-button ${
+						type="button"
+						className={`max-w-lg mx-auto flex flex-col items-center rounded-full bg-sky-400 px-3 py-7 text-sm text-white hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700 ${
 							selectedStatus === 'Waiting' ? 'selected' : ''
 						}`}
 						onClick={() => setSelectedStatus('Waiting')}
 					>
-						Waiting
+						<ClockIcon className="h-8 w-8" aria-hidden="true" />
+
+						<div className="w-24 text-sm mt-2 truncate">รอดำเนินการ</div>
 					</button>
 					<button
-						className={`status-button ${
+						type="button"
+						className={`max-w-lg mx-auto flex flex-col items-center mb-1 rounded-full bg-sky-400 px-3 py-7 text-sm text-white hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700 ${
 							selectedStatus === 'In Progress' ? 'selected' : ''
 						}`}
 						onClick={() => setSelectedStatus('In Progress')}
 					>
-						In Progress
+						<ArrowPathIcon className="h-8 w-8" aria-hidden="true" />
+
+						<div className="w-24 text-sm mt-2 truncate">กำลังดำเนินการ</div>
 					</button>
 					<button
-						className={`status-button ${
+						type="button"
+						className={`max-w-lg mx-auto flex flex-col items-center mb-1 rounded-full bg-sky-400 px-3 py-7 text-sm text-white hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700 ${
 							selectedStatus === 'Complete' ? 'selected' : ''
 						}`}
 						onClick={() => setSelectedStatus('Complete')}
 					>
-						Complete
+						<CheckCircleIcon className="h-8 w-8" aria-hidden="true" />
+
+						<div className="w-24 text-sm mt-2 truncate">เสร็จสิ้น</div>
 					</button>
 				</div>
-				<div className="flex justify-end mt-4">
+				<div className="flex justify-around mt-4">
 					<button
 						className="bg-red-500 text-white px-4 py-2 rounded mr-2"
 						onClick={onClose}
 					>
-						Cancel
+						ยกเลิก
 					</button>
 					<button
 						className="bg-blue-500 text-white px-4 py-2 rounded"
 						onClick={handleUpdateStatus}
 					>
-						OK
+						ยืนยัน
 					</button>
 				</div>
 			</div>
--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,9 +1,7 @@
 import {
-	BuildingStorefrontIcon,
 	CurrencyDollarIcon,
 	DocumentArrowDownIcon,
 	DocumentCheckIcon,
-	DocumentPlusIcon,
 	ShoppingCartIcon,
 	TruckIcon,
 } from '@heroicons/react/24/outline'
@@ -14,79 +12,92 @@ export default function TaskCard() {
 		console.log('Edit button clicked')
 	}
 	return (
-		<div className="bg-sky-200 shadow-md rounded-lg p-3 flex justify-between items-center">
-			<div className="w-3/5 pr-6">
-				<div>
-					<p className="text-sm m-1">ชื่องาน</p>
-					<p className="text-sm m-1">ชื่อหน่วยงาน</p>
-					<p className="text-sm m-1">งบ</p>
-					<p className="text-sm m-1">กำไร</p>
-					<p className="text-sm m-1">วันส่งมอบ</p>
+		<div className="">
+			<div className="bg-sky-200 shadow-md rounded-lg p-3 flex justify-between items-center">
+				<div className="w-3/5 pr-6">
+					<div>
+						<p className="text-sm m-1">ชื่องาน</p>
+						<p className="text-sm m-1">ชื่อหน่วยงาน</p>
+						<p className="text-sm m-1">งบ</p>
+						<p className="text-sm m-1">กำไร</p>
+						<p className="text-sm m-1">วันส่งมอบ</p>
+					</div>
+				</div>
+
+				<div className="flex justify-between items-center space-x-5">
+					{/* Process 1 */}
+					<button
+						type="button"
+						className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
+					>
+						<p className="font-semibold mb-3">การประมูล</p>
+						<DocumentArrowDownIcon className="h-8 w-8" aria-hidden="true" />
+						<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
+							กำลังดำเนินการ
+						</div>
+					</button>
+
+					{/* Process 2 */}
+					<button
+						type="button"
+						className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
+					>
+						<p className="font-semibold mb-3">การเซ็นสัญญา</p>
+
+						<DocumentCheckIcon className="h-8 w-8" aria-hidden="true" />
+						<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
+							รอดำเนินการ
+						</div>
+					</button>
+
+					{/* Process 3 */}
+					<button
+						type="button"
+						className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
+					>
+						<p className="font-semibold mb-3">การสั่งซื้อ</p>
+						<ShoppingCartIcon className="h-8 w-8" aria-hidden="true" />
+						<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
+							เสร็จสิ้น
+						</div>
+					</button>
+
+					{/* Process 4 */}
+					<button
+						type="button"
+						className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
+					>
+						<p className="font-semibold mb-3">การส่งมอบ</p>
+						<TruckIcon className="h-8 w-8" aria-hidden="true" />
+						<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
+							เสร็จสิ้น
+						</div>
+					</button>
+
+					{/* Process 5 */}
+					<button
+						type="button"
+						className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
+					>
+						<p className="font-semibold mb-3">การชำระเงิน</p>
+						<CurrencyDollarIcon className="h-8 w-8" aria-hidden="true" />
+						<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
+							เสร็จสิ้น
+						</div>
+					</button>
 				</div>
 			</div>
-
-			<div className="flex justify-between items-center space-x-5">
-				{/* Process 1 */}
-				<button
-					type="button"
-					className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
-				>
-					<p className="font-semibold mb-3">การประมูล</p>
-					<DocumentArrowDownIcon className="h-8 w-8" aria-hidden="true" />
-					<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
-						กำลังดำเนินการ
-					</div>
-				</button>
-
-				{/* Process 2 */}
-				<button
-					type="button"
-					className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
-				>
-					<p className="font-semibold mb-3">การเซ็นสัญญา</p>
-
-					<DocumentCheckIcon className="h-8 w-8" aria-hidden="true" />
-					<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
-						รอดำเนินการ
-					</div>
-				</button>
-
-				{/* Process 3 */}
-				<button
-					type="button"
-					className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
-				>
-					<p className="font-semibold mb-3">การสั่งซื้อ</p>
-					<ShoppingCartIcon className="h-8 w-8" aria-hidden="true" />
-					<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
-						เสร็จสิ้น
-					</div>
-				</button>
-
-				{/* Process 4 */}
-				<button
-					type="button"
-					className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
-				>
-					<p className="font-semibold mb-3">การส่งมอบ</p>
-					<TruckIcon className="h-8 w-8" aria-hidden="true" />
-					<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
-						เสร็จสิ้น
-					</div>
-				</button>
-
-				{/* Process 5 */}
-				<button
-					type="button"
-					className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
-				>
-					<p className="font-semibold mb-3">การชำระเงิน</p>
-					<CurrencyDollarIcon className="h-8 w-8" aria-hidden="true" />
-					<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
-						เสร็จสิ้น
-					</div>
-				</button>
-			</div>
+			{/* Edit Button */}
+			{/* <button
+				type="button"
+				className="max-w-lg mx-auto flex flex-col items-center mb-1 rounded-md bg-white px-2 py-2 text-sm text-sky-500 hover:bg-sky-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300"
+			>
+				<p className="font-semibold mb-3">การชำระเงิน</p>
+				<CurrencyDollarIcon className="h-8 w-8" aria-hidden="true" />
+				<div className="w-28 text-sm rounded-full bg-sky-500 text-white mt-3 px-2 py-1 truncate">
+					เสร็จสิ้น
+				</div>
+			</button> */}
 		</div>
 	)
 }
